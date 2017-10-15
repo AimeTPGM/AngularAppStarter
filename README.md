a436889681f14519fa97b55c47f9b90bffbd0f97
Pankamol Srikaew <aim.ske09@gmail.com>
2017-10-15 20:07:01 +0700

    now support jade

--- README.md
+++ README.md
@@ -13,3 +13,42 @@ $node app.js & gulp watch
 Then the live-reloadable app will be running on ```localhost:3001``` 
 
 That's it! Enjoy!
+
+# Usage
+
+coming soon
+
+# Note
+
+if you face the following error
+
+```
+Error: listen EADDRINUSE :::3000
+```
+
+it means that your node server is still running on port 3000, you could stop the server by running
+
+```
+killall -9 node
+```
+
+But this will kill **all** node process. If you would like to stop a single process, you can use
+
+```
+lsof -i tcp:3000
+```
+
+it will display something like this
+
+```
+COMMAND   PID     USER   FD   TYPE             DEVICE SIZE/OFF NODE NAME
+node    18734 AimeTPGM   13u  IPv6 0x3027a33fa553d9ed      0t0  TCP *:hbci (LISTEN)
+```
+
+find the command name *node* then kill the process by PID, for example:
+
+```
+kill -9 18734
+```
+
+the node process will be completedly killed
--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,7 @@
  
 const gulp = require('gulp')
 const sass = require('gulp-sass')
+const jade = require('gulp-jade')
 const connect = require('gulp-connect')
 const livereload = require('gulp-livereload')
 const browserSync = require('browser-sync').create()
@@ -18,18 +19,36 @@ gulp.task('browserSync', function() {
 })
 
 gulp.task('sass', function () {
-  return gulp.src('./sass/**/*.sass')
+  return gulp.src('./src/sass/**/*.sass')
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('./public/assets/css'))
     .pipe(browserSync.reload({
       stream: true
     }))
 })
+
+gulp.task('index', function(){
+  return gulp.src('./src/*.jade')
+  .pipe(jade())
+  .pipe(gulp.dest('public/'))
+  .pipe(browserSync.reload({
+    stream: true
+  }))
+})
+
+gulp.task('jade', function(){
+  return gulp.src('./src/views/*.jade')
+  .pipe(jade())
+  .pipe(gulp.dest('public/views'))
+  .pipe(browserSync.reload({
+    stream: true
+  }))
+})
  
 gulp.task('watch', ['browserSync', 'sass'], function () {
-  gulp.watch('./sass/**/*.sass', ['sass'])
+  gulp.watch('./src/sass/**/*.sass', ['sass'])
   gulp.watch('./public/assets/js/**/*.js', browserSync.reload)
-  gulp.watch('./public/*.html', browserSync.reload)
-  gulp.watch('./public/views/*.html', browserSync.reload)
+  gulp.watch('./src/*.jade', ['index'])
+  gulp.watch('./src/views/**/*.jade', ['jade'])
 })
 
